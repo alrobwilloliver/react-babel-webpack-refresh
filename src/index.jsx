6a946ee380122e5a1cf832bf05060506481db18d
Alan Oliver <alrobwilloliver@gmail.com>
2020-08-20 13:34:01 +0100

    added click change color of component class instance method

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = (props) => {
-    return <div>Hello, {props.name}</div>
+// const Hello = (props) => {
+//     return <div>Hello, {props.name}</div>
+// }
+
+// Creating class instantiating an instance of the component
+class Hello extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            clicked: false
+        }
+
+    }
+
+    // Creating an instance method to enable the state of the component (clicked) to be changed on click
+    click = () => {
+        this.setState({
+            clicked: !this.state.clicked
+        })
+    }
+
+    render() {
+        // build and return html for component
+        return (
+            <div onClick={this.click} className={this.state.clicked ? 'clicked' : null}>Hello {this.props.name}</div>
+        )
+    }
 }
 
 const root = document.getElementById('root');
